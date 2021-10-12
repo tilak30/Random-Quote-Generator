74ba6b05840a4a48031ad8d80ce58bc15a15f82f
Tilak <tilakbhansali3009@gmail.com>
2021-10-12 22:48:38 +0530

    MainPage Completed other than hover feature

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,17 +5,19 @@ function MainPage({author, quote, field}) {
     let history = useHistory();
 
     return (
-        <div className="App">
-            <div>
-                {quote}
+        <div className="App" >
+            <div style={{display: "flex", justifyContent:"center", alignItems:"center", marginTop:"7%", marginBottom:"15px"}}>
+                <div style={{width:"100vh", borderLeft:"8px solid #F7DF94", paddingBottom:"15px", paddingTop:"15px"}}>
+                    <div style={{fontSize:"36px", fontWeight:"500",lineHeight:"120%", paddingLeft:"30px"}} >
+                        {quote}
+                    </div>
+                </div>
             </div>
-            <div onClick={() => {
-                history.push("/AuthorQuote");
-            }}>
-                <div>
+            <div style={{display:"flex", flexDirection:"column", justifyContent:"center",marginLeft:"25%", marginTop:"5%"}}>
+                <div style={{fontSize:"24px", fontWeight:"bold", color:"#4F4F4F"}}>
                     {author}
                 </div>
-                <div>
+                <div style={{fontSize:"14px", fontWeight:"500", color:"#828282"}}>
                     {field}
                 </div>
             </div>

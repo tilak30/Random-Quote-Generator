e4cdead487f0a70f85d7240e55f87692bf2b70e9
Tilak <tilakbhansali3009@gmail.com>
2021-10-12 22:47:41 +0530

    Nav updated and random working f9

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MainPage from "./components/MainPage/MainPage";
 import QuoteAuthor from "./components/QuoteAuthor/QuoteAuthor";
-import Random from "./components/Random/Random";
 
 function App() {
-  const [quote, setQuote] = useState("Most people never run far enough on their first wind to find out they've got a second.");
-  const [author, setAuthor] = useState("William James");
-  const [field, setField] = useState("sports");
+  const [quote, setQuote] = useState("“The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency. The second is that automation applied to an inefficient operation will magnify the inefficiency.”");
+  const [author, setAuthor] = useState("Bill Gates");
+  const [field, setField] = useState("business");
 
   const handleClick = () => {
 
@@ -23,7 +22,10 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Random handleClick={handleClick} />
+        <div onClick={handleClick} className="header" style={{display:"flex", width:"100%", justifyContent:"end", marginRight:"10px", marginTop:"10px"}}>
+          <div style={{paddingRight:"10px", fontSize:"20px"}}>random</div>
+          <img style={{paddingRight:"10px", height:"22px"}} src="https://img.icons8.com/ios-glyphs/50/000000/refresh--v2.png"/>
+        </div>
         <Switch>
           <Route exact path="/" >
             <MainPage author={author} quote={quote} field={field} />
